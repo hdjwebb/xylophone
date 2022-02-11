df6333f8a0e2bbe791c8b34c4181ad98c50782d9
Henry Webb <henry@ifpossible.co.uk>
2022-02-11 13:53:53 +0000

    finished project - probably should have commited sooner!

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        print(sender.currentTitle)
+        playSound(sender.currentTitle!)
+        UIView.animate(withDuration: 0.2) {
+            sender.alpha = 0.2
+//          print("Start"
+        }
+        
+        UIView.animate(withDuration: 0.2) {
+            sender.alpha = 1
+//          print("End")
+        }
+        
+        
+        
+    }
     
-
+    func playSound(_ resourceName: String) {
+        let url = Bundle.main.url(forResource: resourceName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
